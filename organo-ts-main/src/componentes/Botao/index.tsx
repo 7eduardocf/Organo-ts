4ead41fc3cb858a8d58786e39436585d58ad7c76
Eduardo Frota <eduardocfrota901@gmail.com>
2025-04-16 22:38:23 -0400

    feita a migração do botao e do componente banner para tsx

--- organo-ts-main/src/componentes/Botao/index.tsx
+++ organo-ts-main/src/componentes/Botao/index.tsx
@@ -1,6 +1,12 @@
+import { ReactElement } from 'react'
 import './Botao.css'
+import React from "react"
 
-const Botao = (props) => {
+interface BotaoProps{
+    children: ReactElement
+}
+
+const Botao = (props : BotaoProps) => {
     return (<button className='botao'>
         {props.children}
     </button>)
