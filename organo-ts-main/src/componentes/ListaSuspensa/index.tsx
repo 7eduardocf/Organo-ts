e95a2d8f50b284d1cb6d06dc02d342fa96695f38
Eduardo Frota <eduardocfrota901@gmail.com>
2025-04-18 21:46:17 -0400

    feita a migração da lista suspensa

--- organo-ts-main/src/componentes/ListaSuspensa/index.tsx
+++ organo-ts-main/src/componentes/ListaSuspensa/index.tsx
@@ -1,10 +1,20 @@
 import './ListaSuspensa.css'
 
-const ListaSuspensa = (props) => {
+interface ListaSuspensaProps{
+    label: string
+    aoAlterado: (valor: string) => void
+    required: boolean
+    valor: string
+    itens: string[]
+}
+
+const ListaSuspensa = (props: ListaSuspensaProps) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)}
+                required={props.required}
+                value={props.valor}>
                 <option value=""></option>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
